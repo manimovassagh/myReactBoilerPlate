91031358d668b14baccf7a723dbaa9b48259effd
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2021-01-09 17:25:28 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import "./App.css";
+
+
+class App extends React.Component {
+ 
+    render() { 
+        return (
+        <div className="App">
+            <h1>Hi mani finally made it and debug <br/>from stackoverflow also</h1>
+            <h2>mani you did it</h2>
+        </div>
+            );
+    }
+}
+ 
+export default App;
